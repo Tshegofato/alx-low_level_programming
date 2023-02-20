cfa1339140afeeee6f799fa943bf8d94bd85ab93
Tshegofato <basetsana_12@icloud.com>
2023-02-20 09:00:42 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 1 (Success)
+ * Return: Always 1
  */
+
 int main(void)
 {
 	write(2,"and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
-	return (1);
+	return (1)
 }
