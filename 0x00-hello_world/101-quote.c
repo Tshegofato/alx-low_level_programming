af8c20e09b415be6d375020755724a0dfbc6f704
Tshegofato <basetsana_12@icloud.com>
2023-02-20 09:43:34 -0800

    Quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry
  * Return: 1
@@ -8,4 +8,4 @@ int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
-} 
+}	
