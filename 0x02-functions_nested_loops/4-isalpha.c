67fd10fed5666143195cd7d8f3d9189bcb6952c7
Tshegofato <basetsana_12@icloud.com>
2023-02-23 16:42:16 -0800

    Sign

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,12 @@
 #include "main.h"
 
 /**
- * _isalpha - functions that check for alphabets
- *
- * @c: parameter to be checked
- *
- * Return: 1 if it is an alphabet
- * and 0 otherwise
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
  */
-
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || ((c >= 65 && c <= 90))
-			return (1);
-			else
-			return (0);
-			}
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
